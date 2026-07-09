02ddad09166678d0ee1b6bdf2c7bde703f6b757c
Alex <oleks9185@gmail.com>
2026-07-09 22:59:32 +0200

    Overwiev section (#28)
    * css
    
    * overviewSection

---
+++ bus-tracker-frontend/src/components/OverviewSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OverviewSection from './OverviewSection';
+
+
+<OverviewSection
+    selectedStop={selectedStop}
+    onSelectStop={selectStop}
+    savedStops={savedStops}
+    onSave={handleSave}
+    onDelete={handleDelete}
+    overviewRef={overviewRef}
+/>
